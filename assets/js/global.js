03fcab1a5c643d8725f73ec79800b83a297919ba
Braydn Larsen <braydnl@gmail.com>
2023-09-13 10:05:16 -0700

    change contract

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,8 +1,8 @@
 const { SecretNetworkClient, MsgExecuteContract } = window.secretjs;
 
 
-const lottery_contract =  "secret127x38dzzz8xcur8uyk38sujcxdhuln527lnm83";
-const lottery_hash =  "fac3fa71ca5759dedac3a03f23dba6a081778c948ac61be978faf260fa743346";
+const lottery_contract =  "secret166j6uauqd59zndlsu4trjjhh0r2myr94y4azht";
+const lottery_hash =  "2b477beff2793a26950bae3c159ad3cb0d946fce07b56933d467e15d008f9f58";
 const sscrt_contract = 'secret1gvn6eap7xgsf9kydgmvpqwzkru2zj35ar2vncj';
 const sscrt_hash = 'c74bc4b0406507257ed033caa922272023ab013b0c74330efc16569528fa34fe';
 
