fad0ccc4ec2eaf8c56ba8c55f8216f1c208d1cc8
Braydn Larsen <braydnl@gmail.com>
2024-01-29 14:55:53 -0800

    mainnet

--- public/js/global.js
+++ public/js/global.js
@@ -1,10 +1,10 @@
 const { SecretNetworkClient, MsgExecuteContract } = window.secretjs;
 
 
-const zoo_contract =  "secret1mj9utpjltxsdgcamjmsujq8tnezrk543ppk0rj";
-const zoo_hash =  "00d7881854b00f7214c3ae1c275ca2ae16f79be6f52946a7c9bc5ef2b17b70e5";
-const snip_contract = 'secret1p6r5zc8898c9h3zfssfxu2x75nz3t4z8q68w8t';
-const snip_hash = 'c74bc4b0406507257ed033caa922272023ab013b0c74330efc16569528fa34fe';
+const zoo_contract =  "secret1hqt9aus6v8fmlj9zzhzek2mh2u8qfazl0khrzr";
+const zoo_hash =  "ecdeedee3debd2d1a6aa22b80d499a0a58096922dcceac12c0a7e22712145a71";
+const snip_contract = 'secret1fl449muk5yq8dlad7a22nje4p5d2pnsgymhjfd';
+const snip_hash = '638a3e1d50175fbcb8373cf801565283e3eb23d88a9b7b7f99fcc5eb1e6b561e';
 const decimal = 1000000;
 
 let viewing_key;
@@ -18,96 +18,27 @@ window.addEventListener("keplr_keystorechange", () => {
     console.log("changed accounts")
     location.reload(true);
 })
+
+            
 async function connectKeplr() {
-    this.chainId = 'pulsar-3';
 
-    // Keplr extension injects the offline signer that is compatible with cosmJS.
-    // You can get this offline signer from `window.getOfflineSigner(chainId:string)` after load event.
-    // And it also injects the helper function to `window.keplr`.
-    // If `window.getOfflineSigner` or `window.keplr` is null, Keplr extension may be not installed on browser.
-    if (!window.getOfflineSigner || !window.keplr) {
-        alert("Please install keplr extension");
-    } else {
-        if (window.keplr.experimentalSuggestChain) {
-            try {
-                // Setup Secret Testnet (not needed on mainnet)
-                // Keplr v0.6.4 introduces an experimental feature that supports the feature to suggests the chain from a webpage.
-                // cosmoshub-3 is integrated to Keplr so the code should return without errors.
-                // The code below is not needed for cosmoshub-3, but may be helpful if you’re adding a custom chain.
-                // If the user approves, the chain will be added to the user's Keplr extension.
-                // If the user rejects it or the suggested chain information doesn't include the required fields, it will throw an error.
-                // If the same chain id is already registered, it will resolve and not require the user interactions.
-                await window.keplr.experimentalSuggestChain({
-                    chainId: this.chainId,
-                    chainName: 'Secret Testnet',
-                    rpc: 'https://rpc.pulsar.scrttestnet.com',
-                    rest: "https://api.pulsar.scrttestnet.com",
-                    bip44: {
-                        coinType: 529,
-                    },
-                    coinType: 529,
-                    stakeCurrency: {
-                        coinDenom: 'SCRT',
-                        coinMinimalDenom: 'uscrt',
-                        coinDecimals: 6,
-                    },
-                    bech32Config: {
-                        bech32PrefixAccAddr: 'secret',
-                        bech32PrefixAccPub: 'secretpub',
-                        bech32PrefixValAddr: 'secretvaloper',
-                        bech32PrefixValPub: 'secretvaloperpub',
-                        bech32PrefixConsAddr: 'secretvalcons',
-                        bech32PrefixConsPub: 'secretvalconspub',
-                    },
-                    currencies: [
-                        {
-                            coinDenom: 'SCRT',
-                            coinMinimalDenom: 'uscrt',
-                            coinDecimals: 6,
-                        },
-                    ],
-                    feeCurrencies: [
-                        {
-                            coinDenom: 'SCRT',
-                            coinMinimalDenom: 'uscrt',
-                            coinDecimals: 6,
-                        },
-                    ],
-                    gasPriceStep: {
-                        low: 0.1,
-                        average: 0.25,
-                        high: 0.4,
-                    },
-                    features: ['secretwasm'],
-                });
+    this.chainId = 'secret-4';
 
-                // This method will ask the user whether or not to allow access if they haven't visited this website.
-                // Also, it will request user to unlock the wallet if the wallet is locked.
-                // If you don't request enabling before usage, there is no guarantee that other methods will work.
-                await window.keplr.enable(this.chainId);
+    await window.keplr.enable(chainId);
 
-                // @ts-ignore
-                const keplrOfflineSigner = window.getOfflineSignerOnlyAmino(this.chainId);
-                const accounts = await keplrOfflineSigner.getAccounts();
-                
-                this.address = accounts[0].address;
+    const keplrOfflineSigner = window.keplr.getOfflineSignerOnlyAmino(chainId);
+    const accounts = await keplrOfflineSigner.getAccounts();
+    this.address = accounts[0].address;
 
-                window.secretjs = new SecretNetworkClient({
-                  url: "https://api.pulsar.scrttestnet.com/",
-                  chainId: this.chainId,
-                  wallet: keplrOfflineSigner,
-                  walletAddress: this.address,
-                  encryptionUtils: window.keplr.getEnigmaUtils(this.chainId),
-                });
-            } catch (error) {
-                console.error(error)
-            }
-        } else {
-            alert("Please use the recent version of keplr extension");
-        }
-    }
+    const url = "https://lcd.secret.express";
 
-
+    window.secretjs = new SecretNetworkClient({
+        url,
+        chainId: this.chainId,
+        wallet: keplrOfflineSigner,
+        walletAddress: this.address,
+        encryptionUtils: window.keplr.getEnigmaUtils(chainId),
+    });
 
     if (this.address) {
         try {
@@ -119,3 +50,4 @@ async function connectKeplr() {
         console.log("error connecting to keplr");
     }
 }
+
